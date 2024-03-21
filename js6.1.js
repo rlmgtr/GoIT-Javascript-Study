e8d7693b95a7bf0125a873a64b19d3dfee31ddcb
rlmgtr <rlmagtira@gmail.com>
2024-03-21 15:33:33 +0800

    study

--- js6.1.js
+++ js6.1.js
@@ -1,10 +1,12 @@
+
+
 let heading = document.getElementById("heading");
 // console.log(heading);
 
 heading.style.color = "blue";
+// ID is for single elements
 
-
-let headers = document.getElementsByClassName('header');
+//let headers = document.getElementsByClassName('header');
 // console.log(headers); will give you an array
 headers[0].style.color = "red";
 headers[1].style.color = "red";
@@ -13,3 +15,15 @@ headers[2].style.color = "red";
 for (let i = 0; i = headers.length; i++){
     headers[i].style.color = "red";
 }
+
+// above can use for and for of
+
+// class is for multiple similar elements, but adding a function will not be similar to class in CSS, but LOOPS must be applied in such instances. See above for example. * take note that this happens because classes are viewed by js as an array. 
+
+// querySelector can only target one even there are many classes:
+
+let headingID = document.querySelector('#heading') // for ID - gives nodelist
+let headersQ = document.querySelectorAll('.header') // for class gives html collection
+
+// html is a LIVE collection while node is STATIC
+

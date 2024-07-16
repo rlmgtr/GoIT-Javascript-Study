78e09f3fff65e3fa63402c5595a887b753d8fb5b
rlmgtr <rlmagtira@gmail.com>
2024-07-16 23:10:59 +0800

    update

--- Promises/Promises.js
+++ Promises/Promises.js
@@ -1,8 +1,23 @@
-new Promise ((resolve, reject) => {
+
+
+let testPromise = new Promise ((resolve, reject) => {
 let value = 7;
 setTimeout(() => {
 value = value + 3
-},1000)
 
+
+if (value === 10) {
+    resolve("This is enough");
+} else { 
+    reject("This is NOT enough"); }
+
+},1000);
 
 })
+
+console.log(testPromise);
+
+// Promise has 3 states
+// 1. Pending
+// 2. Fulfilled
+// 3. Rejected

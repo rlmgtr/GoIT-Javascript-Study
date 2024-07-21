5a6d788aeb796c820baa5e1cf29514671a1a0f6f
rlmgtr <rlmagtira@gmail.com>
2024-07-21 16:58:44 +0800

    update

--- Promises/promises3.js
+++ Promises/promises3.js
@@ -1,1 +1,16 @@
-cons
+
+let card = 100;
+let bank = 100;
+let fundsAvailable = 100
+
+
+
+
+const promisePayment = new Promise ((Success, Failed)=>{
+if  ()  { Success
+
+
+} else Failed (()=>{}) 
+
+
+})

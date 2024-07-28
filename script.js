25667b3bdf80c5041cb0d415acceadff7bf2e472
rlmgtr <rlmagtira@gmail.com>
2024-07-28 11:51:46 +0800

    update / study

---
+++ script.js
@@ -0,0 +1,17 @@
+import { getNews } from "./news-api.js";
+
+let newsForm = document.getElementById("newsForm");
+
+newsForm.addEventListener('submit', exeSubmit);
+
+function exeSubmit(e) {
+    e.preventDefault();
+
+    let form = e.target; 
+    const topic = form.elements.newsInput.value;
+    getNews(topic).then((data) => console.log(data));
+}
+
+
+ 
+

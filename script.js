61bc29337b90d126d61d8e244fc9c9c8ae720f41
rlmgtr <rlmagtira@gmail.com>
2024-07-28 15:04:48 +0800

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { getNews } from "./news-api.js";
+pimport { getNews } from "./news-api.js";
 
 let newsForm = document.getElementById("newsForm");
 
@@ -11,9 +11,35 @@ function exeSubmit(e) {
     const topic = form.elements.newsInput.value;
 
 
-    getNews(topic).then((data) => console.log(data));
+    getNews(topic).then((data) => {
+ const { articles } = data;
+
+ if (articles.lenght === 0) throw new Error("sorry, No articles found");
+
+ return articles.map(createMarkup);
+    });
 }
 
-// resume at 1:28
+function createMarkup ({author, title, description, url, urlToImage}) {
+
+return `
+<div class='article-card'>
+<h2 class='article-title'><h2>
+<h3 class='article author'></h3>
+<img />
+<p class='article-description></p>
+<a>Read more</a>
+</div>
+`;
+
+function updateNewsList () {
+    document.getElementById('newsDisplay')
+}
+
+
+function onError
+
+
+} 
  
 

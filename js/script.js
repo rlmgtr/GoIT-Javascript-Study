72c2add2d13db6391a6c134f88be3a5addb8a080
rlmgtr <rlmagtira@gmail.com>
2024-08-29 20:40:44 +0800

    update - will copy from prev

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = "https://newsapi.org/v2/everything";
-const API_KEY = "75bee3a37c9e4fe5b9a56aca9eda806e";
-
-function getNews(searchQuery, queryPage) {
-    const url = `${BASE_URL}?q=${searchQuery}&pageSize=5&page${queryPage}`;
-
-
-};

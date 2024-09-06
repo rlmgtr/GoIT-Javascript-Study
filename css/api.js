dbd573f4854e148f91d55cb8aa49241b2cf757a9
rlmgtr <rlmagtira@gmail.com>
2024-09-06 15:23:47 +0800

    update

---
+++ css/api.js
@@ -0,0 +1,16 @@
+console Base_URL = "https://66d7f5af37b1cadd8052cde0.mockapi.io";
+
+// get all todos
+export const getToDo = () => {
+    return fetch(Base_URL).then(res => res.JSON();)
+    
+};
+
+// add a to do 
+export const addToDo = () => {};
+
+// delete a to do 
+export const deleteToDo = () => {};
+
+// update a to do
+export const updateToDo = () => {};
--- css/script.js
+++ css/script.js
@@ -1,2 +1,40 @@
+import { addToDo, deleteToDo, updateToDo, getToDo } from "./api";
+
+const addToDoBtn = document.getElementById("addToDoBtn")
+
+const myUL = documen.getElementById("myUL")
+
+const toDoInput = documen.getElementById("toDoInput")
+
+letCurrentID = 1;
+
+function createLi(toDoText,isDone, id ) {
+let li = document.getElementById("li") // creates an li
+li.innerText = text; // <li>code<li> injetcs the input
+li.dataset.id = id; // <li data-id="1">code</li>
+myUL.appendChild(li)
+
+if (isDone) li.classname = "checked"; // write a css for this
+
+};
+
+
+
+function fillTodoList() {
+    getToDo().then(toDos => {
+       toDos.forEach(({toDoText, isDone, id}) => createLi(toDoText, isDone, id));
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
 // https://www.youtube.com/watch?v=56BGgrVolkA&t=3894s
-// 25:00
+// 47:52
